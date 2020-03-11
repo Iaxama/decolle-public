62454fc59a6783697b22f3f9e2bf4956ec650883
Massimiliano Iacono <massimiliano.iacono@iit.it>
2020-03-11 17:05:46 +0100

    generate_params now accepts an arbitrary number of param/value pairs

--- scripts/generate_params.py
+++ scripts/generate_params.py
@@ -1,13 +1,13 @@
 import yaml
 import os
 import argparse
-
+import glob
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Parameter file generator for grid search')
-    parser.add_argument('--param_name', '-p', dest='param_name', type=str, required=True,
+    parser.add_argument('--param_name', '-p', dest='param_name', type=str, required=True, action='append',
                         help='Name of parameter to change as it appears in default_params_file')
-    parser.add_argument('--values_list', '-v', dest='values_list', required=True, nargs='+',
+    parser.add_argument('--values_list', '-v', dest='values_list', required=True, nargs='+', action='append',
                         help='Set of values to test for the specified parameter. By default parsed as int. '
                              'If float add decimal (i.e -v 2.0). If list type comma separated entries in turn separated by space.'
                              'Single element list must have a comma at the end (i.e -v 1,2,3 4, 5,6')
@@ -22,47 +22,69 @@ def parse_args():
 def main():
     args = parse_args()
 
-    with open(args.default_params_file, 'r') as f:
-        params = yaml.load(f, Loader=yaml.FullLoader)
-
     out_dir = 'params_to_test'
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
+    else:
+        [os.remove(x) for x in glob.glob(os.path.join(out_dir, '*'))]
 
+    param_list = [args.default_params_file]
+
+    for param_name, values in zip(args.param_name, args.values_list):
+        for param_file in param_list:
+            with open(param_file, 'r') as f:
+                params = yaml.load(f, Loader=yaml.FullLoader)
+            generate_param_test(out_dir, params, param_name, values)
+        param_list = [os.path.join(out_dir, x) for x in os.listdir(out_dir)]
+        if not param_list:
+            param_list = [args.default_params_file]
+
+
+def generate_param_test(out_dir, params, param_name, values_list):
     t = None
     subt = None
-    for i, p in enumerate(args.values_list):
+    for p in values_list:
         # Parsing values
         params_out = params.copy()
         p_split = p.split(',')
-        if len(p_split) > 1:
+        if len(p_split) > 1 or isinstance(params_out[param_name], list):
             if not p_split[-1]:
                 del p_split[-1]
             try:
-                params_out[args.param_name] = [int(e) for e in p_split]
+                new_value = [int(e) for e in p_split]
             except ValueError:
-                params_out[args.param_name] = [float(e) for e in p_split]
+                new_value = [float(e) for e in p_split]
+
         else:
             try:
-                params_out[args.param_name] = int(p_split[0])
+                new_value = int(p_split[0])
             except ValueError:
-                params_out[args.param_name] = float(p_split[0])
-        with open(os.path.join(out_dir, '{}.yml'.format(i)), 'w') as outfile:
+                new_value = float(p_split[0])
+
+        if params_out[param_name] == new_value:
+            continue  # Avoid duplicates
+
+        params_out[param_name] = new_value
+        try:
+            last_idx = int(os.path.splitext(sorted(os.listdir(out_dir))[-1])[0])
+        except IndexError:
+            last_idx = 0
+        with open(os.path.join(out_dir, '{:05d}.yml'.format(last_idx + 1)), 'w') as outfile:
             yaml.dump(params_out, outfile)
 
         # Checking type consistency
         warn = False
 
-        if isinstance(params_out[args.param_name], list):
+        if isinstance(params_out[param_name], list):
             if subt is not None:
-                if not isinstance(params_out[args.param_name][0], subt):
+                if not isinstance(params_out[param_name][0], subt):
                     warn = True
-            subt = type(params_out[args.param_name][0])
+            subt = type(params_out[param_name][0])
 
         if t is not None:
-            if not isinstance(params_out[args.param_name], t):
+            if not isinstance(params_out[param_name], t):
                 warn = True
-        t = type(params_out[args.param_name])
+        t = type(params_out[param_name])
 
         if warn:
             print('WARNING! Check type consistency')
